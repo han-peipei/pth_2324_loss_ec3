3044d1611d5266b3c5f7deccd10cc2729f3a1c88
han-peipei <82298194+han-peipei@users.noreply.github.com>
2026-05-11 23:33:55 +0800

    Update main_3_B.sh

--- main_3_B.sh
+++ main_3_B.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -euo pipefail
-path=/kaggle/working/pth_2324_loss_ec2/
+path=/kaggle/working/pth_2324_loss_ec3/
 mkdir -p "$path"
 # 可选：线程数（Kaggle 不一定有 64 核）
 # export OMP_NUM_THREADS=8
